18ff7bc62640cc67cd0c26226080ef3600cf9a41
souzamtech <matheussouza102030@gmail.com>
2026-06-18 00:50:50 +0000

    Alterações da aula 17/06 e criação da atividade6

---
+++ Atividade 6- crachá/cracha.js
@@ -0,0 +1,8 @@
+function gerarCracha() {
+    window.alert("Crachá virtual Operante!")
+    let nome = document.getElementById("nome").value;
+
+    console.log("Crachá existente!", nome)
+
+    alert("Crachá existente.\n\n O senhor está validado" \n\n )
+}
---
+++ Atividade5/formulario.js
@@ -0,0 +1,9 @@
+
+function imprimirNome() {
+    window.alert("Usuário cadastrado")
+    let nome = document.getElementById("nome").value;
+
+    console.log("Seu nome é: ", nome)
+
+    alert("Aluno cadastrado!\n\n Nome do aluno: " + nome)
+}
