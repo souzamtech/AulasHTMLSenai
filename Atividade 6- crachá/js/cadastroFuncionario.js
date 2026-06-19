07001d98b2f237ed44165dcc2952f013a8c798c9
souzamtech <matheussouza102030@gmail.com>
2026-06-19 00:50:20 +0000

    Atividade 6 18/06- funcionario e crachá

---
+++ Atividade 6- crachá/js/cadastroFuncionario.js
@@ -0,0 +1,9 @@
+function cadastrarFuncionario() {
+    let nome = document.getElementById("nome-funcionario").value;
+    let horas = document.getElementById("horas-funcionario").value;
+    let cargo = document.getElementById("cargo-funcionario").value;
+
+    console.log(nome)
+    console.log(horas)
+    console.log(cargo)
+}
---
+++ Atividade 6- crachá/js/cracha.js
@@ -0,0 +1,19 @@
+function gerarCracha() {
+    window.alert("Crachá virtual Operante!")
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("Idade").value;
+    let cargo = document.getElementById("cargo").value;
+    let Departamento = document.getElementById("Departamento").value;
+    let acessoRestrito = document.getElementById("acessoRestrito").value;
+
+
+
+    console.log("Crachá existente!")
+    console.log("nome", nome)
+    console.log("idade", idade)
+    console.log("cargo", cargo)
+    console.log("Departamento", Departamento)
+  
+
+    alert("Crachá existente.\n\n O senhor está validado \n\n")
+}
